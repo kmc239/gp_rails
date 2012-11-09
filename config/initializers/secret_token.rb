9ca391f35373cc69ea17e9330478d710c8ee5946
kmc239 <kyle.clegg@gmail.com>
2012-11-09 06:44:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gp::Application.config.secret_token = '9ab67e1f4dd7f4007a579e5137d5879f46bffd54f8cbffa70540a96f166f874b560d77a3e8e04b59aaceb67fd5336c147dab7a496cb3aa7339f233d237f0c142'
