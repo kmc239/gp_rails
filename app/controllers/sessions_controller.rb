01a0caa61d59cca05ed52c6a1e8d1440076bd46d
kmc239 <kyle.clegg@gmail.com>
2012-11-27 06:30:43 -0700

    Updated Login API to return user object upon success. This allows us to save an API call to getUser and to be more RESTful.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
   
-  # Login API that returns a 200 if valid creds, else a 403 forbidden
+  # Login API that returns a 204 if valid creds, else a 403 forbidden
   def create
     @user = User.authenticate(params[:email], params[:password])
     
     respond_to do |format|
       if @user
         format.html { redirect_to @user, notice: 'User was successfully logged in.' }
-        format.json { head :no_content, :status => 200 }
+        format.json { render json: @user, :status => 200 }
       else
         flash.now.alert = "Invalid email or password"
         format.html { render action: "new" }
