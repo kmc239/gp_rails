a390b70c9bfc7f41ea84c615fc7dbf5470b127b3
kmc239 <kyle.clegg@gmail.com>
2012-11-26 23:25:05 -0700

    Minor db schema change.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126092517) do
+ActiveRecord::Schema.define(:version => 20121127054956) do
+
+  create_table "app_users", :force => true do |t|
+    t.string   "first_name",             :default => "", :null => false
+    t.string   "last_name",              :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "app_users", ["email"], :name => "index_app_users_on_email", :unique => true
+  add_index "app_users", ["first_name"], :name => "index_app_users_on_first_name"
+  add_index "app_users", ["last_name"], :name => "index_app_users_on_last_name"
+  add_index "app_users", ["reset_password_token"], :name => "index_app_users_on_reset_password_token", :unique => true
 
   create_table "entries", :force => true do |t|
     t.text     "description"
